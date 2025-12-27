e77c1e7bdf7a72907b3a17526341fb82e3a4fef3
Adhish Thite <adhish.thite@elastic.co>
2025-12-27 22:50:35 +0530

    style: adjust navbar layout for improved positioning and aesthetics
    - Changed the navbar's position from fixed to absolute for better alignment.
    - Updated the bottom margin to enhance spacing and visual hierarchy.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,8 +13,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-16 flex origin-bottom h-full max-h-14">
-      <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
+    <div className="pointer-events-none fixed inset-x-0 bottom-16 z-30 mx-auto flex h-14">
+      <div className="absolute -bottom-16 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
       <Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_4px_6px_rgba(0,0,0,.07),0_12px_24px_rgba(0,0,0,.08)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
         {DATA.navbar.map((item) => (
           <DockIcon key={item.href}>
